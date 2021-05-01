a1bd02ba934784e34d1602c2a25598c7719aee0c
pascal <pmhuberts@gmail.com>
2021-05-01 15:40:31 +0200

    first commit

---
+++ src/Providers/HueServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace WeDevelop4You\LaravelHue\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class HueServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+	/**
+	 * Bootstrap services.
+	 *
+	 * @return void
+	 */
+    public function boot()
+    {
+
+    }
+}
